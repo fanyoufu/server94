0c2ec5060f3c178f2f30c8f3d6c450ac4043fe63
fanyoufu <fanyoufu@outlook.com>
2020-02-11 17:06:56 +0800

    2.11下午

--- code/nodejs/14两种导出模块的方法/module1.js
+++ code/nodejs/14两种导出模块的方法/module1.js
@@ -5,14 +5,15 @@
 // exports = {
 //     a:1
 // }
-exports.a = 1;
+// exports.a = 1;
+module.exports.a = 1;
 
 // exports= {a: 100}
 // module.exports.b= 200;
 
 // console.log(exports === module.exports); //true
-// console.log(module.exports); 
+console.log(module.exports); 
 // console.log(exports); 
 // // module.exports 初始是一个空对象
 // // module.exports = 1
-// console.log( 'module1.js代码在运行')
+console.log( 'module1.js代码在运行')
