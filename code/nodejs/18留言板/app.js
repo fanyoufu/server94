98b5348aaa2b90ad21d1364ba10501ddd417b0ac
fanyoufu <fanyoufu@outlook.com>
2020-02-13 12:03:22 +0800

    完成留言板的改造

--- code/nodejs/18留言板/app.js
+++ code/nodejs/18留言板/app.js
@@ -7,7 +7,7 @@ const http = require('http')
 const fs = require('fs')
 const path = require('path')
 const url = require('url')
-
+const querystring = require('querystring')
 // 引入自定义模块
 // 用来操作message.json
 const msg = require("./msg")
@@ -37,6 +37,28 @@ const server = http.createServer((req, res) => {
         }  
         res.setHeader("content-type","application/json;charset=utf-8")
         res.end(JSON.stringify( result) )
+    } else if(obj.pathname ==="/addmsg" && req.method === "POST"){
+        // 1. 获取post过来的参数
+        let result = "";
+        req.on("data",buf=>{
+            result +=buf
+        })
+        req.on('end',()=>{
+            // 把查询字符串转成对象
+            // let obj = querystring.parse(result)
+            let {name,content} = querystring.parse(result)
+            // console.log("post收到的参数",result,obj);
+            // 2. 保存到 message.json， 来实现添加功能
+            msg.add(name,content)
+            let rs = {
+                code: 200,
+                msg:'添加成功'
+            }
+            res.setHeader('content-type','application/json;charset=utf-8')
+            res.end( JSON.stringify(rs) )
+        })
+
+
     } else {
         // 如果 不是接口 ，就直接去public94下读文件
 
